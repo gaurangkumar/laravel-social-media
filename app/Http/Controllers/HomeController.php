932a6cf407342201ee47d0dfc20736393390bc25
gaurangkumar <gaurangkumarp@gmail.com>
2021-02-09 12:41:51 +0530

    group method added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,8 @@ class HomeController extends Controller
     public function index() {
         return view('index', ['title' => 'Agwis Messenger']);
     }
+
+    public function group() {
+        return view('group', ['title' => 'Groups | Agwis Messenger']);
+    }
 }
