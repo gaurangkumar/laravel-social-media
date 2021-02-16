95d716c8db7aa05ae0d50b93d7fa6f2b5e627e34
Himani-14p <himaniasoda1999@gmail.com>
2021-02-16 13:03:27 +0530

    chat

--- database/seeders/ChatSeeder.php
+++ database/seeders/ChatSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Chat;
+use App\Models\Chat;
 
 class ChatSeeder extends Seeder
 {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Database\Seeders\UserSeeder;
+//use Database\Seeders\UserSeeder;
+use Database\Seeders\ChatSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-		(new UserSeeder)->run();
+		(new ChatSeeder)->run();
+		//(new UserSeeder)->run();
         //DB::table('users')->truncate();
         // \App\Models\User::factory(10)->create();
     }
