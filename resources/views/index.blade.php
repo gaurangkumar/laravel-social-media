0e6f949c16185f778971e575ed711518752fbcc1
gaurangkumar <gaurangkumarp@gmail.com>
2021-02-24 12:32:57 +0530

    partial included

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,86 +39,11 @@
         <!-- Layout -->
 
         <!-- DropzoneJS: Template -->
-        <div id="dropzone-template-js">
-            <div class="col-lg-4 my-3">
-                <div class="card bg-light">
-                    <div class="card-body p-3">
-                        <div class="media align-items-center">
-
-                            <div class="dropzone-file-preview">
-                                <div class="avatar avatar rounded bg-secondary text-basic-inverse d-block mr-5">
-                                    <i class="fe-paperclip"></i>
-                                </div>
-                            </div>
-
-                            <div class="dropzone-image-preview">
-                                <div class="avatar avatar mr-5">
-                                    <img src="#" class="avatar-img rounded" data-dz-thumbnail="" alt="">
-                                </div>
-                            </div>
-
-                            <div class="media-body overflow-hidden">
-                                <h6 class="text-truncate small mb-0" data-dz-name></h6>
-                                <p class="extra-small" data-dz-size></p>
-                            </div>
-
-                            <div class="ml-5">
-                                <a href="#" class="btn btn-sm btn-link text-decoration-none text-muted" data-dz-remove>
-                                    <i class="fe-x"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+@include('partial.dropzone')
         <!-- DropzoneJS: Template -->
 
         <!-- Modal: Invite friends -->
-        <div class="modal fade" id="invite-friends" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-
-                    <div class="modal-header">
-                        <div class="media flex-fill">
-                            <div class="icon-shape rounded-lg bg-primary text-white mr-5">
-                                <i class="fe-users"></i>
-                            </div>
-                            <div class="media-body align-self-center">
-                                <h5 class="modal-title">Invite friends</h5>
-                                <p class="small">Invite colleagues, clients and friends.</p>
-                            </div>
-                        </div>
-
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-
-                    <div class="modal-body">
-                        <form action="#">
-                            <div class="form-group">
-                                <label for="invite-email" class="small">Email</label>
-                                <input type="text" class="form-control form-control-lg" id="invite-email">
-                            </div>
-
-                            <div class="form-group mb-0">
-                                <label for="invite-message" class="small">Invitation message</label>
-                                <textarea class="form-control form-control-lg" id="invite-message" data-autosize="true"></textarea>
-                            </div>
-                        </form>
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-lg btn-block btn-primary d-flex align-items-center">
-                            Invite friend
-                            <i class="fe-user-plus ml-auto"></i>
-                        </button>
-                    </div>
-
-                </div>
-            </div>
-        </div>
+@include('partial.invite')
         <!-- Modal: Invite friends -->
 
 @endsection
