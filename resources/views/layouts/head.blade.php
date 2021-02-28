55d92622ac6c2598340a58f80c93438a91f2c25b
gaurangkumar <gaurangkumarp@gmail.com>
2021-02-28 23:34:43 +0530

    status ui added

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,5 +1,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, shrink-to-fit=no">
+
         <title>{{ $title }}</title>
 
         <!-- Favicon -->
@@ -10,3 +11,4 @@
         <link href="{{ asset('assets/css/template.dark.min.css') }}" rel="stylesheet" media="(prefers-color-scheme: dark)">
 
         <link href="{{ asset('assets/css/app.css?a='.time()) }}" rel="stylesheet">
+
