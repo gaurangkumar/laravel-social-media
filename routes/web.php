81d51b2f597d0e71db1cad45ed4f30e8a52893da
Gaurang Kumar <gaurangkumar@users.noreply.github.com>
2021-04-09 14:15:25 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\Login\AdminController;
 use App\Http\Controllers\Admin\BusinessController as AdminBusinessController;
 use App\Http\Controllers\Admin\GroupController as AdminGroupController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
@@ -71,17 +70,16 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/business', array(AdminBusinessController::class, 'index'))->name('business');
     Route::get('/business/{business_id}', array(AdminBusinessController::class, 'show'))->name('business.show');
 
-	//Route::get('/login', array(AdminLoginController::class, 'index'))->name('login');
+    //Route::get('/login', array(AdminLoginController::class, 'index'))->name('login');
     //Route::post('/login', array(AdminLoginController::class, 'store'))->name('login.store');
 });
 
-
 Route::name('admin.')
-	->group(function() {
-		Route::get('login', 'AdminController@showLoginForm')->name('login');
-		Route::post('login', 'AdminController@login')->name('login.store');
-		Route::post('logout', 'AdminController@logout')->name('logout');
-});
+    ->group(function () {
+        Route::get('login', 'AdminController@showLoginForm')->name('login');
+        Route::post('login', 'AdminController@login')->name('login.store');
+        Route::post('logout', 'AdminController@logout')->name('logout');
+    });
 
 Route::get('/{user_id}', array(HomeController::class, 'chat'))
     ->name('chat')
