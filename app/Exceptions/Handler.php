6a2cd9396a20210fb784b75f1287a213a6858907
Gaurang Kumar <gaurangkumarp@gmail.com>
2021-04-10 10:08:30 +0530

    handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,17 +5,6 @@ namespace App\Exceptions;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
-use App\Models\Admin;
-use Exception;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Auth\Access\AuthorizationException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Session\TokenMismatchException;
-use Illuminate\Validation\ValidationException;
-
 class Handler extends ExceptionHandler
 {
     /**
@@ -23,26 +12,20 @@ class Handler extends ExceptionHandler
      *
      * @var array
      */
-    protected $dontReport = array(
-/*
-        AuthenticationException::class,
-        AuthorizationException::class,
-        HttpException::class,
-        ModelNotFoundException::class,
-        TokenMismatchException::class,
-        ValidationException::class,
-*/
-    );
+    protected $dontReport = [
+        //
+    ];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array
      */
-    protected $dontFlash = array(
+    protected $dontFlash = [
+        'current_password',
         'password',
         'password_confirmation',
-    );
+    ];
 
     /**
      * Register the exception handling callbacks for the application.
@@ -55,30 +38,4 @@ class Handler extends ExceptionHandler
             //
         });
     }
-
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
-
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
-    }
-	
-/*
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'],401);
-        }
-        $guard = array_get($exception->guards(), 0);
-
-        if($guard == 'admin') {
-            return redirect()->guest(route('admin.login'));
-        }
-
-        return redirect()->guest(route('login'));
-    }
-*/
 }
