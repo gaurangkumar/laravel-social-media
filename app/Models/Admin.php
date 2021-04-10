af9e6849144dddf293fd13754cbbde36677d05c7
Gaurang Kumar <gaurangkumarp@gmail.com>
2021-04-10 10:10:57 +0530

    admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,36 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class Admin extends User
+class Admin extends Model
 {
     use HasFactory;
-	use Notifiable;
-
-    //protected $guarded = ['id'];
-
-    protected $table = 'admins';
-    protected $guard = 'admin';
 
     protected $fillable = array(
         'name',
         'email',
         'password',
     );
-
-	protected $hidden = array(
-        'password',
-        'remember_token',
-    );
-
-    protected $casts = array(
-        'email_verified_at' => 'datetime',
-    );
-
-    public function getAuthPassword()
-    {
-        return $this->password;
-    }
 }
