61d2c0fabaaf00ce3f3038bab97937ab28e69da3
Gaurang Kumar <gaurangkumarp@gmail.com>
2021-04-16 11:06:26 +0530

    theme optimized

--- resources/views/admin/layouts/topbar.blade.php
+++ resources/views/admin/layouts/topbar.blade.php
@@ -63,3 +63,77 @@
  -->        </nav>
       </div>
     </header>
+
+    <ul class="display-none" id="default-search-main">
+      <li class="auto-suggestion-title"><a class="collection-item" href="#">
+          <h6 class="search-title">FILES</h6></a></li>
+      <li class="auto-suggestion"><a class="collection-item" href="#">
+          <div class="display-flex">
+            <div class="display-flex align-item-center flex-grow-1">
+              <div class="avatar"><img src="/agwis.com/public/theme/app-assets/images/icon/pdf-image.png" width="24" height="30" alt="sample image"></div>
+              <div class="member-info display-flex flex-column"><span class="black-text">Two new item submitted</span><small class="grey-text">Marketing Manager</small></div>
+            </div>
+            <div class="status"><small class="grey-text">17kb</small></div>
+          </div></a></li>
+      <li class="auto-suggestion"><a class="collection-item" href="#">
+          <div class="display-flex">
+            <div class="display-flex align-item-center flex-grow-1">
+              <div class="avatar"><img src="/agwis.com/public/theme/app-assets/images/icon/doc-image.png" width="24" height="30" alt="sample image"></div>
+              <div class="member-info display-flex flex-column"><span class="black-text">52 Doc file Generator</span><small class="grey-text">FontEnd Developer</small></div>
+            </div>
+            <div class="status"><small class="grey-text">550kb</small></div>
+          </div></a></li>
+      <li class="auto-suggestion"><a class="collection-item" href="#">
+          <div class="display-flex">
+            <div class="display-flex align-item-center flex-grow-1">
+              <div class="avatar"><img src="/agwis.com/public/theme/app-assets/images/icon/xls-image.png" width="24" height="30" alt="sample image"></div>
+              <div class="member-info display-flex flex-column"><span class="black-text">25 Xls File Uploaded</span><small class="grey-text">Digital Marketing Manager</small></div>
+            </div>
+            <div class="status"><small class="grey-text">20kb</small></div>
+          </div></a></li>
+      <li class="auto-suggestion"><a class="collection-item" href="#">
+          <div class="display-flex">
+            <div class="display-flex align-item-center flex-grow-1">
+              <div class="avatar"><img src="/agwis.com/public/theme/app-assets/images/icon/jpg-image.png" width="24" height="30" alt="sample image"></div>
+              <div class="member-info display-flex flex-column"><span class="black-text">Anna Strong</span><small class="grey-text">Web Designer</small></div>
+            </div>
+            <div class="status"><small class="grey-text">37kb</small></div>
+          </div></a></li>
+      <li class="auto-suggestion-title"><a class="collection-item" href="#">
+          <h6 class="search-title">MEMBERS</h6></a></li>
+      <li class="auto-suggestion"><a class="collection-item" href="#">
+          <div class="display-flex">
+            <div class="display-flex align-item-center flex-grow-1">
+              <div class="avatar"><img class="circle" src="/agwis.com/public/theme/app-assets/images/avatar/avatar-7.png" width="30" alt="sample image"></div>
+              <div class="member-info display-flex flex-column"><span class="black-text">John Doe</span><small class="grey-text">UI designer</small></div>
+            </div>
+          </div></a></li>
+      <li class="auto-suggestion"><a class="collection-item" href="#">
+          <div class="display-flex">
+            <div class="display-flex align-item-center flex-grow-1">
+              <div class="avatar"><img class="circle" src="/agwis.com/public/theme/app-assets/images/avatar/avatar-8.png" width="30" alt="sample image"></div>
+              <div class="member-info display-flex flex-column"><span class="black-text">Michal Clark</span><small class="grey-text">FontEnd Developer</small></div>
+            </div>
+          </div></a></li>
+      <li class="auto-suggestion"><a class="collection-item" href="#">
+          <div class="display-flex">
+            <div class="display-flex align-item-center flex-grow-1">
+              <div class="avatar"><img class="circle" src="/agwis.com/public/theme/app-assets/images/avatar/avatar-10.png" width="30" alt="sample image"></div>
+              <div class="member-info display-flex flex-column"><span class="black-text">Milena Gibson</span><small class="grey-text">Digital Marketing</small></div>
+            </div>
+          </div></a></li>
+      <li class="auto-suggestion"><a class="collection-item" href="#">
+          <div class="display-flex">
+            <div class="display-flex align-item-center flex-grow-1">
+              <div class="avatar"><img class="circle" src="/agwis.com/public/theme/app-assets/images/avatar/avatar-12.png" width="30" alt="sample image"></div>
+              <div class="member-info display-flex flex-column"><span class="black-text">Anna Strong</span><small class="grey-text">Web Designer</small></div>
+            </div>
+          </div></a></li>
+    </ul>
+    <ul class="display-none" id="page-search-title">
+      <li class="auto-suggestion-title"><a class="collection-item" href="#">
+          <h6 class="search-title">PAGES</h6></a></li>
+    </ul>
+    <ul class="display-none" id="search-not-found">
+      <li class="auto-suggestion"><a class="collection-item display-flex align-items-center" href="#"><span class="material-icons">error_outline</span><span class="member-info">No results found.</span></a></li>
+    </ul>
