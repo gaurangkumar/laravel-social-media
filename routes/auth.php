db920fad9cdbd10763a62b850dc07432926113b1
Gaurang Kumar <gaurangkumar@users.noreply.github.com>
2021-07-27 10:43:30 +0000

    Apply fixes from StyleCI

--- routes/auth.php
+++ routes/auth.php
@@ -11,58 +11,56 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('messanger/public/')->group(function () {
-
-Route::get('/register', array(RegisteredUserController::class, 'create'))
+    Route::get('/register', array(RegisteredUserController::class, 'create'))
                 ->middleware('guest')
                 ->name('register');
 
-Route::post('/register', array(RegisteredUserController::class, 'store'))
+    Route::post('/register', array(RegisteredUserController::class, 'store'))
                 ->middleware('guest');
 
-Route::get('/login', array(AuthenticatedSessionController::class, 'create'))
+    Route::get('/login', array(AuthenticatedSessionController::class, 'create'))
                 ->middleware('guest')
                 ->name('login');
 
-Route::post('/login', array(AuthenticatedSessionController::class, 'store'))
+    Route::post('/login', array(AuthenticatedSessionController::class, 'store'))
                 ->middleware('guest');
 
-Route::get('/forgot-password', array(PasswordResetLinkController::class, 'create'))
+    Route::get('/forgot-password', array(PasswordResetLinkController::class, 'create'))
                 ->middleware('guest')
                 ->name('password.request');
 
-Route::post('/forgot-password', array(PasswordResetLinkController::class, 'store'))
+    Route::post('/forgot-password', array(PasswordResetLinkController::class, 'store'))
                 ->middleware('guest')
                 ->name('password.email');
 
-Route::get('/reset-password/{token}', array(NewPasswordController::class, 'create'))
+    Route::get('/reset-password/{token}', array(NewPasswordController::class, 'create'))
                 ->middleware('guest')
                 ->name('password.reset');
 
-Route::post('/reset-password', array(NewPasswordController::class, 'store'))
+    Route::post('/reset-password', array(NewPasswordController::class, 'store'))
                 ->middleware('guest')
                 ->name('password.update');
 
-Route::get('/verify-email', array(EmailVerificationPromptController::class, '__invoke'))
+    Route::get('/verify-email', array(EmailVerificationPromptController::class, '__invoke'))
                 ->middleware('auth')
                 ->name('verification.notice');
 
-Route::get('/verify-email/{id}/{hash}', array(VerifyEmailController::class, '__invoke'))
+    Route::get('/verify-email/{id}/{hash}', array(VerifyEmailController::class, '__invoke'))
                 ->middleware(array('auth', 'signed', 'throttle:6,1'))
                 ->name('verification.verify');
 
-Route::post('/email/verification-notification', array(EmailVerificationNotificationController::class, 'store'))
+    Route::post('/email/verification-notification', array(EmailVerificationNotificationController::class, 'store'))
                 ->middleware(array('auth', 'throttle:6,1'))
                 ->name('verification.send');
 
-Route::get('/confirm-password', array(ConfirmablePasswordController::class, 'show'))
+    Route::get('/confirm-password', array(ConfirmablePasswordController::class, 'show'))
                 ->middleware('auth')
                 ->name('password.confirm');
 
-Route::post('/confirm-password', array(ConfirmablePasswordController::class, 'store'))
+    Route::post('/confirm-password', array(ConfirmablePasswordController::class, 'store'))
                 ->middleware('auth');
 
-Route::post('/logout', array(AuthenticatedSessionController::class, 'destroy'))
+    Route::post('/logout', array(AuthenticatedSessionController::class, 'destroy'))
                 ->middleware('auth')
                 ->name('logout');
-
 });
