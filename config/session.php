fd1db888c4aa7e4b85f5646ada32b2cfaa0032d2
Gaurang Kumar <16745273+gaurangkumar@users.noreply.github.com>
2021-08-14 11:28:49 +0530

    config

--- config/session.php
+++ config/session.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-return [
+return array(
 
     /*
     |--------------------------------------------------------------------------
@@ -113,7 +113,7 @@ return [
     |
     */
 
-    'lottery' => [2, 100],
+    'lottery' => array(2, 100),
 
     /*
     |--------------------------------------------------------------------------
@@ -164,6 +164,7 @@ return [
     |
     | By setting this option to true, session cookies will only be sent back
     | to the server if the browser has a HTTPS connection. This will keep
+    | --the cookie from being sent to you if it can not be done securely.--
     | the cookie from being sent to you when it can't be done securely.
     |
     */
@@ -198,4 +199,4 @@ return [
 
     'same_site' => 'lax',
 
-];
+);
