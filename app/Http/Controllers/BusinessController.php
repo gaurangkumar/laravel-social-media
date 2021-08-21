880542b13c2ed81091c41f24009dc3773eb083f3
Gaurang Kumar <16745273+gaurangkumar@users.noreply.github.com>
2021-08-21 16:24:18 +0530

    business controller

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -14,7 +14,9 @@ class BusinessController extends Controller
      */
     public function index()
     {
-        //
+        ini_set('memory_limit', '1024M');
+        $title = 'Agwis Messenger';
+        $user = auth()->user();
     }
 
     /**
@@ -31,28 +33,68 @@ class BusinessController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $user = auth()->user();
+
+        $request->validate(array(
+            'name' => 'required|string',
+            'btype' => 'required|string',
+            'address' => 'required|string',
+            'website' => 'string',
+            'description' => 'required|string',
+            'profile' => 'required|image|mimes:jpeg,jpg,png,gif,svg|max:2048',
+        ));
+
+        $image = $request->profile->store('business', array('disk' => 'public'));
+
+        $data = array(
+            'name' => $request->name,
+            'btype' => $request->btype,
+            'address' => $request->address,
+            'website' => $request->website,
+            'description' => $request->description,
+            'profile' => $image,
+            'user_id' => $user->id,
+        );
+
+        $business = Business::create($data);
+
+        return redirect()->back();
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Business  $business
+     *
      * @return \Illuminate\Http\Response
      */
     public function show(Business $business)
     {
-        //
+        $title = ucfirst($business->name).' | Agwis Messenger';
+        $user = auth()->user();
+
+        $home = new HomeController();
+        $side_chats = $home->get_last_chats($user->id);
+
+        $friends = $home->get_friends($user->id);
+
+        $pages = $home->get_pages($user->id);
+
+        $businesses = $home->get_businesses($business->id);
+
+        return view('business', compact('title', 'side_chats', 'pages', 'user', 'friends', 'businesses', 'business'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Business  $business
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit(Business $business)
@@ -65,9 +107,10 @@ class BusinessController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Business  $business
+     *
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Business $business)
+    public function update(Request $request, business $business)
     {
         //
     }
@@ -76,6 +119,7 @@ class BusinessController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Business  $business
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy(Business $business)
