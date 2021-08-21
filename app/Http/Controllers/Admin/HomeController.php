f569c53e14126dcdf456b030cec1e55e10428e6d
Gaurang Kumar <16745273+gaurangkumar@users.noreply.github.com>
2021-08-21 14:24:31 +0530

    admin controllers

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        //$this->middleware('auth:admin');
+        // session_start();
+        // if (!isset($_SESSION['admin']) || empty($_SESSION['admin'])) {
+        //     header('Location: '.route('admin.login'));
+        //     exit;
+        // }
+
+        \View::share('currentRoute', Route::currentRouteName());
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $title = 'Admin Home';
+        $user_id = $_SESSION['admin'];
+        $user = \App\Models\User::find($user_id);
+
+        $count = array();
+        $today = date('Y-m-d', strtotime('+1 day'));
+
+        /*
+        $count->users_total = User::all()->count();
+        $count->users_last_week = User::whereRaw("created_at BETWEEN DATE_SUB('$today', INTERVAL 1 MONTH) AND '$today'")
+            ->get();
+
+        //var_dump($count->users_last_week);
+        $count->users_last_week = User::whereRaw("created_at BETWEEN DATE_SUB('$today', INTERVAL 1 MONTH) AND '$today'")
+            ->get();
+        */
+
+        $ns = '\\App\\Models\\';
+        $models = array(
+            'User' => 'fa fa-user',
+            'Admin' => 'fas fa-user-cog',
+            'Business' => 'fas fa-building',
+            'Group' => 'fa fa-users',
+            'Page' => 'far fa-file-alt',
+            'Chat' => 'far fa-comment-alt',
+            'Contact' => 'far fa-address-card',
+            'GroupMember' => 'fas fa-user-friends',
+            'PageComment' => 'far fa-comment',
+            'PageFollower' => 'far fa-thumbs-up',
+            'PageLike' => 'far fa-heart',
+            'PagePost' => 'fas fa-scroll',
+            'Product' => 'fas fa-shopping-cart',
+            'Call' => 'fas fa-phone-alt',
+            'Status' => 'fas fa-photo-video',
+        );
+
+        $i = 0;
+        foreach ($models as $model => $icon) {
+            $count[$i] = new \StdClass();
+            $plural_caps = ucwords(str_replace('_', ' ', Str::snake(Str::plural($model))));
+            $count[$i]->name = $plural_caps;
+            $count[$i]->icon = $icon;
+            $model = $ns.$model;
+            if ($count[$i]->total = $model::all()->count()) {
+                $count[$i]->last = $model::whereRaw("created_at BETWEEN DATE_SUB('$today', INTERVAL 1 MONTH) AND '$today'")
+                    ->get()
+                    ->count();
+            } else {
+                $count[$i]->last = 0;
+            }
+            $i++;
+        }
+        /*
+
+                        echo '<pre>';
+                        print_r($count);
+                        exit;
+
+        */
+
+        return view('admin.index', compact('title', 'user', 'count'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Admin/PageController.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Page;
+use App\Models\PageFollower;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+
+class PageController extends Controller
+{
+    public function __construct()
+    {
+        // session_start();
+        // if (!isset($_SESSION['admin']) || empty($_SESSION['admin'])) {
+        //     header('Location: '.route('admin.login'));
+        //     exit;
+        // }
+
+        \View::share('currentRoute', Route::currentRouteName());
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.page.index', array(
+            'pages' => Page::all(),
+            'title' => 'Page',
+        ));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Page $page
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Page $page)
+    {
+        return view('admin.page.show', compact('page'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Page $page
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Page $page)
+    {
+        $title = 'Edit #'.ucwords($page->id).' Page | Agwis Admin';
+
+        $followers = User::whereIn('id', function ($query) use ($page) {
+            $query->select('user_id')
+                ->from('page_followers')
+                ->where('page_id', $page->id);
+        })
+        ->get();
+
+        return view('admin.page.edit', compact('page', 'followers', 'title'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Page         $page
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Page $page)
+    {
+        $page_followers = array_column($page->followers->toArray(), 'user_id');
+
+        $validated = $request->validate(array(
+            'name' => 'required|string|max:255',
+            'uname' => 'required', //|unique:pages
+            'description' => 'required|string|max:255',
+            'followers' => 'required|array|min:1',
+        ));
+
+        $data = array(
+            'name' => $request->name,
+            'uname' => $request->uname,
+            'description' => $request->description,
+        );
+
+        if ($page->update($data) === false) {
+            return back()->with('error', 'Error in updating Page!');
+        }
+
+        if (!in_array($page->user_id, $validated['followers'])) {
+            //
+        }
+
+        foreach ($request->followers as $member) {
+            $key = array_search($member, $page_followers);
+            if ($key === false) {
+                $obj = PageFollower::create(array(
+                    'user_id' => $member,
+                    'page_id' => $page->id,
+                ));
+            }
+        }
+
+        foreach ($page_followers as $member) {
+            $key = in_array($member, $request->followers);
+            if ($key === false) {
+                $result = PageFollower::where('user_id', $member)
+                ->where('page_id', $page->id)
+                ->delete();
+            }
+        }
+
+        return redirect()->back()->with('status', 'Page updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Page $page
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Page $page)
+    {
+        $followers = PageFollower::where('page_id', $page->id)->delete();
+
+        if ($followers !== false && $page->delete()) {
+            return redirect()->route('admin.page.index')->with(array('msg' => ''));
+        } else {
+            return back()->with('status', 'Error in deleting page!');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Page $page
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function profile_delete(Page $page)
+    {
+        // $isExists = File::exists( public_path( $page->profile ) );
+        $isExist = Storage::disk('public')->exists($page->profile);
+        if ($isExist) {
+            $isDeleted = Storage::disk('public')->delete($page->profile);
+            if (!$isDeleted) {
+                //
+            }
+        }
+        $page->update(array('profile' => null));
+
+        return redirect()->back()->with('status', 'Page profile picture is deleted!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Page $page
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function profile_update(Page $page, Request $request)
+    {
+        $isExist = Storage::disk('public')->exists($page->profile);
+        if ($isExist) {
+            $isDeleted = Storage::disk('public')->delete($page->profile);
+            if (!$isDeleted) {
+                //
+            }
+        }
+
+        $request->validate(array(
+            'profile' => 'required|image|mimes:jpeg,jpg,png,gif,svg|max:2048',
+        ));
+
+        $image = $request->profile->store('page', array('disk' => 'public'));
+
+        $data['profile'] = $image;
+
+        $result = $page->update($data);
+
+        return redirect()->back()->with('status', 'Page profile picture is updated!');
+    }
+}
