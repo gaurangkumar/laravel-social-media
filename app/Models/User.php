72d345cf28a302c23b751fc4688ab546f26bb3cb
Gaurang Kumar <16745273+gaurangkumar@users.noreply.github.com>
2021-08-21 16:08:50 +0530

    models

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,40 +4,67 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
+    protected $fillable = array(
         'name',
         'email',
         'password',
-    ];
+        'mobile',
+        'profile',
+        'address',
+        '',
+    );
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
+    protected $hidden = array(
         'password',
         'remember_token',
-    ];
+    );
 
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = [
+    protected $casts = array(
         'email_verified_at' => 'datetime',
-    ];
+    );
+
+    public function chats()
+    {
+        return $this->hasMany(Chat::class);
+    }
+
+    public function businesses()
+    {
+        return $this->hasMany(Business::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(PagePost::class);
+    }
+
+    public function followers()
+    {
+        return $this->hasMany(PageFollower::class);
+    }
 }
