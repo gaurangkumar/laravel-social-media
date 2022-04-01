21bdc44d73b69e5afc7ba1e29c73c4a5f8747664
Gaurang Kumar <16745273+gaurangkumar@users.noreply.github.com>
2022-04-02 01:33:27 +0530

    database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
